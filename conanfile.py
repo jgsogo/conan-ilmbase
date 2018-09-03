1d25d8d247b845d3ced017e75d82e5dfd1631a93
Javier G. Sogo <jgsogo@gmail.com>
2018-09-03 20:40:51 +0200

    up to version 2.3.0 and simplify recipe

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ import os
 class IlmBaseConan(ConanFile):
     name = "IlmBase"
     description = "IlmBase is a component of OpenEXR. OpenEXR is a high dynamic-range (HDR) image file format developed by Industrial Light & Magic for use in computer imaging applications."
-    version = "2.2.0"
+    version = "2.3.0"
     license = "BSD"
     url = "https://github.com/Mikayex/conan-ilmbase.git"
     settings = "os", "compiler", "build_type", "arch"
@@ -23,46 +23,31 @@ class IlmBaseConan(ConanFile):
             self.options.fPIC = True
 
     def source(self):
-        tools.download("http://download.savannah.nongnu.org/releases/openexr/ilmbase-%s.tar.gz" % self.version,
-                       "ilmbase.tar.gz")
-        tools.untargz("ilmbase.tar.gz")
-        os.unlink("ilmbase.tar.gz")
+        url = "https://github.com/openexr/openexr/releases/download/v{version}/ilmbase-{version}.tar.gz"
+        tools.get(url.format(version=self.version))
         tools.replace_in_file("ilmbase-%s/CMakeLists.txt" % self.version, "PROJECT ( ilmbase )",
                               """PROJECT ( ilmbase )
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()""")
 
-        # Remove tests compilation
-        tools.replace_in_file("ilmbase-%s/CMakeLists.txt" % self.version, "ADD_SUBDIRECTORY ( HalfTest )", "")
-        tools.replace_in_file("ilmbase-%s/CMakeLists.txt" % self.version, "ADD_SUBDIRECTORY ( IexTest )", "")
-        tools.replace_in_file("ilmbase-%s/CMakeLists.txt" % self.version, "ADD_SUBDIRECTORY ( ImathTest )", "")
-
-    def build(self):
+    def _configure_cmake(self):
         cmake = CMake(self)
+        cmake.definitions["ENABLE_TESTS"] = False
         cmake.definitions["NAMESPACE_VERSIONING"] = self.options.namespace_versioning
         if "fPIC" in self.options.fields:
             cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
 
         src_dir = "ilmbase-%s" % self.version
         cmake.configure(source_dir=src_dir)
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include/OpenEXR", src="ilmbase-%s/Half" % self.version, keep_path=False)
-        self.copy("*.h", dst="include/OpenEXR", src="ilmbase-%s/Iex" % self.version, keep_path=False)
-        self.copy("*.h", dst="include/OpenEXR", src="ilmbase-%s/IexMath" % self.version, keep_path=False)
-        self.copy("*.h", dst="include/OpenEXR", src="ilmbase-%s/IlmThread" % self.version, keep_path=False)
-        self.copy("*.h", dst="include/OpenEXR", src="ilmbase-%s/Imath" % self.version, keep_path=False)
-        self.copy("IlmBaseConfig.h", dst="include/OpenEXR", src="config", keep_path=False)
-
-        self.copy("*.lib", dst="lib", src=".", keep_path=False)
-        self.copy("*.a", dst="lib", src=".", keep_path=False)
-        self.copy("*.so", dst="lib", src=".", keep_path=False)
-        self.copy("*.so.*", dst="lib", src=".", keep_path=False)
-        self.copy("*.dylib*", dst="lib", src=".", keep_path=False)
-
-        self.copy("*.dll", dst="bin", src="bin", keep_path=False)
-
+        cmake = self._configure_cmake()
+        cmake.install()
         self.copy("FindIlmBase.cmake", src=".", dst=".")
         self.copy("license*", dst="licenses", src="ilmbase-%s" % self.version, ignore_case=True, keep_path=False)
 
